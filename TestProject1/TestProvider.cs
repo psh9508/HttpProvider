c2304d82b6f64f0b192db7393cdefd71ae740ccc
Masuri <psh0258@gmail.com>
2022-10-25 00:13:57 +0900

    Test 객체 만드는 중

--- TestProject1/TestProvider.cs
+++ TestProject1/TestProvider.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HttpProvider;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,12 @@ namespace TestProject1
 {
     internal class TestProvider
     {
+        private readonly IHttpHeader _httpProvider;
+
+        public TestProvider()
+        {
+            // Injection 객체 넣어줘야 함. 좋은 설계가 맞는 것인가?
+            //_httpProvider = new HttpProvider.HttpProvider();
+        }
     }
 }
