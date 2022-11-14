d46644aaa45cff4ca0993caf3c79db08a435a742
Masuri <psh0258@gmail.com>
2022-11-14 22:28:56 +0900

    _httpClient를 1개만 만들어서 공유하기 위해 base class 제작

---
+++ HttpProvider/Bases/HttpProviderBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpProvider.Bases
+{
+    public abstract class HttpProviderBase
+    {
+        public static HttpClient _httpClient;
+
+        static HttpProviderBase()
+        {
+            _httpClient = new HttpClient();
+        }
+    }
+}
